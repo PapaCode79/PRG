f309dd65c51bfd4287a7e8bf2ce25f1cb98734b6
badara <pushsoccer79@gmail.com>
2022-05-05 23:40:01 -0400

    first commit

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 // TODO: Include packages needed for this application
-
+const fs = require("fs")
+const inquirer = require("inquirer")
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message:"what is the title for your project"
+    },
+    {
+        type: "input",
+        name: "des",
+        message:"what is the description of your project",
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    return inquirer.prompt(questions)
+        .then(function (answers) {
+                console.log(answers)
+            })
+}
 
 // Function call to initialize app
 init();
